7400b8c312981c0d9f40362e0498b1cc81648283
liujingshi <1471703928@qq.com>
2019-12-31 00:07:35 +0800

    update echarts

--- template/pc/iframe/home/charts.js
+++ template/pc/iframe/home/charts.js
@@ -1,5 +1,6 @@
 var chart_pie_use = echarts.init(document.getElementById("chart_pie_use"));
 var chart_pie_unuse = echarts.init(document.getElementById("chart_pie_unuse"));
+var chart_pie_state = echarts.init(document.getElementById("chart_pie_state"));
 
 // 在用资产饼状图
 var chart_pie_use_option = {
@@ -109,5 +110,46 @@ var chart_pie_unuse_option = {
     }]
 };
 
+// 资产状态占比饼状图
+var chart_pie_state_option = {
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)",
+        confine: true
+    },
+    series: [{
+        name: '资产状态占比',
+        type: 'pie',
+        radius:'70%',
+        center: ['30%', '50%'],
+        avoidLabelOverlap: false,
+        label: {
+            show: false
+        },
+        data: [{
+                value: 335,
+                name: '电脑'
+            },
+            {
+                value: 310,
+                name: '电视'
+            },
+            {
+                value: 234,
+                name: '广告牌'
+            },
+            {
+                value: 135,
+                name: '照相机'
+            },
+            {
+                value: 1548,
+                name: '折叠椅'
+            }
+        ]
+    }]
+};
+
 chart_pie_use.setOption(chart_pie_use_option);
 chart_pie_unuse.setOption(chart_pie_unuse_option);
+chart_pie_state.setOption(chart_pie_state_option);
