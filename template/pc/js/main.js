6b933117490d4e87c5b331997398dc4b73b4c2ca
liujingshi <1471703928@qq.com>
2019-12-31 13:26:45 +0800

    update nav left

--- template/pc/js/main.js
+++ template/pc/js/main.js
@@ -1,22 +1,36 @@
+var nav_left = new Vue({
+    el: "#nav_left",
+    data: {
+        navs: nav_left_data
+    }
+});
+
 $(".nav-pull").click(function () {
     var that = this;
-    $(".nav-pull").each(function () {
-        if (this != that) {
-            $(this).parent().find(".nav-ul-son").css("display", "none");
-            $(this).find(".pull-icon").removeClass("la-angle-down");
-            $(this).find(".pull-icon").addClass("la-angle-right");
-        }
+    $(this).parent().parent().children().each(function () {
+        $(this).children().each(function () {
+            var cs = $(this).attr("class");
+            var css = cs.split(" ");
+            if (css.indexOf("nav-pull") >= 0) {
+                if (this != that) {
+                    $(this).parent().children().eq(1).css("display", "none");
+                    $(this).find(".pull-icon").removeClass("la-angle-down");
+                    $(this).find(".pull-icon").addClass("la-angle-right");
+                } else {
+                    var $ul = $(this).parent().children().eq(1);
+                    if ($ul.css("display") == "none") {
+                        $ul.css("display", "flex");
+                        var $pull = $(this).find(".pull-icon");
+                        $pull.removeClass("la-angle-right");
+                        $pull.addClass("la-angle-down");
+                    } else {
+                        $ul.css("display", "none");
+                        var $pull = $(this).find(".pull-icon");
+                        $pull.removeClass("la-angle-down");
+                        $pull.addClass("la-angle-right");
+                    }
+                }
+            }
+        });
     });
-    var $ul = $(this).parent().find(".nav-ul-son");
-    if ($ul.css("display") == "none") {
-        $ul.css("display", "flex");
-        var $pull = $(this).find(".pull-icon");
-        $pull.removeClass("la-angle-right");
-        $pull.addClass("la-angle-down");
-    } else {
-        $ul.css("display", "none");
-        var $pull = $(this).find(".pull-icon");
-        $pull.removeClass("la-angle-down");
-        $pull.addClass("la-angle-right");
-    }
 });
---
+++ template/pc/js/nav.js
@@ -0,0 +1,246 @@
+var nav_left_data = [{
+        title: "首页",
+        icon: "las la-home",
+        pull: false,
+        href: "index.html",
+        active: true,
+        son: []
+    },
+    {
+        title: "资产管理",
+        icon: "las la-coins",
+        pull: true,
+        href: "javascript:;",
+        active: false,
+        son: [{
+                title: "资产入库",
+                icon: "las la-shuttle-van",
+                pull: false,
+                href: "javascript:;",
+                active: false,
+                son: []
+            },
+            {
+                title: "领用&退库",
+                icon: "las la-plus-circle",
+                pull: false,
+                href: "javascript:;",
+                active: false,
+                son: []
+            },
+            {
+                title: "借用&归还",
+                icon: "las la-plus-square",
+                pull: false,
+                href: "javascript:;",
+                active: false,
+                son: []
+            },
+            {
+                title: "资产调拨",
+                icon: "las la-sync-alt",
+                pull: false,
+                href: "javascript:;",
+                active: false,
+                son: []
+            },
+            {
+                title: "实物信息更变",
+                icon: "las la-edit",
+                pull: false,
+                href: "javascript:;",
+                active: false,
+                son: []
+            },
+            {
+                title: "维保信息更变",
+                icon: "las la-briefcase-medical",
+                pull: false,
+                href: "javascript:;",
+                active: false,
+                son: []
+            },
+            {
+                title: "财务信息更变",
+                icon: "las la-yen-sign",
+                pull: false,
+                href: "javascript:;",
+                active: false,
+                son: []
+            },
+            {
+                title: "维修信息登记",
+                icon: "las la-wrench",
+                pull: false,
+                href: "javascript:;",
+                active: false,
+                son: []
+            },
+            {
+                title: "清理报废",
+                icon: "las la-trash-alt",
+                pull: false,
+                href: "javascript:;",
+                active: false,
+                son: []
+            },
+            {
+                title: "盘点管理",
+                icon: "las la-file-invoice",
+                pull: false,
+                href: "javascript:;",
+                active: false,
+                son: []
+            },
+            {
+                title: "分析报表",
+                icon: "las la-chart-bar",
+                pull: true,
+                href: "javascript:;",
+                active: false,
+                son: [{
+                        title: "资产清单",
+                        icon: "las la-file-alt",
+                        pull: false,
+                        href: "javascript:;",
+                        active: false,
+                        son: []
+                    },
+                    {
+                        title: "资产履历",
+                        icon: "las la-file-alt",
+                        pull: false,
+                        href: "javascript:;",
+                        active: false,
+                        son: []
+                    },
+                    {
+                        title: "员工资产统计",
+                        icon: "las la-file-alt",
+                        pull: false,
+                        href: "javascript:;",
+                        active: false,
+                        son: []
+                    },
+                    {
+                        title: "标准资产型号统计",
+                        icon: "las la-file-alt",
+                        pull: false,
+                        href: "javascript:;",
+                        active: false,
+                        son: []
+                    },
+                    {
+                        title: "闲置资产共享",
+                        icon: "las la-file-alt",
+                        pull: false,
+                        href: "javascript:;",
+                        active: false,
+                        son: []
+                    }
+                ]
+            },
+            {
+                title: "设置",
+                icon: "las la-cog",
+                pull: true,
+                href: "javascript:;",
+                active: false,
+                son: [{
+                        title: "资产分类",
+                        icon: "las la-list-ul",
+                        pull: false,
+                        href: "javascript:;",
+                        active: false,
+                        son: []
+                    },
+                    {
+                        title: "区域",
+                        icon: "las la-dot-circle",
+                        pull: false,
+                        href: "javascript:;",
+                        active: false,
+                        son: []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        title: "耗材管理",
+        icon: "las la-industry",
+        pull: true,
+        href: "javascript:;",
+        active: false,
+        son: [{
+                title: "入库单",
+                icon: "las la-cloud-download-alt",
+                pull: false,
+                href: "javascript:;",
+                active: false,
+                son: []
+            },
+            {
+                title: "出库单",
+                icon: "las la-cloud-upload-alt",
+                pull: false,
+                href: "javascript:;",
+                active: false,
+                son: []
+            },
+            {
+                title: "设置",
+                icon: "las la-cog",
+                pull: true,
+                href: "javascript:;",
+                active: false,
+                son: [{
+                    title: "物品档案",
+                    icon: "las la-list-ul",
+                    pull: false,
+                    href: "javascript:;",
+                    active: false,
+                    son: []
+                }]
+            }
+        ]
+    },
+    {
+        title: "财务管理",
+        icon: "las la-comment-dollar",
+        pull: true,
+        href: "javascript:;",
+        active: false,
+        son: [{
+            title: "结账",
+            icon: "las la-hand-point-right",
+            pull: false,
+            href: "javascript:;",
+            active: false,
+            son: []
+        }]
+    },
+    {
+        title: "我的审批",
+        icon: "las la-stamp",
+        pull: false,
+        href: "javascript:;",
+        active: false,
+        son: []
+    },
+    {
+        title: "系统管理",
+        icon: "las la-server",
+        pull: true,
+        href: "javascript:;",
+        active: false,
+        son: [{
+            title: "系统用户",
+            icon: "las la-user",
+            pull: false,
+            href: "javascript:;",
+            active: false,
+            son: []
+        }]
+    }
+];
