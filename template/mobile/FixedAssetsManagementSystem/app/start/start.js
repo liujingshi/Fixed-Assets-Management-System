43584ff358ace53a7ce5064b0672f8876a611a43
liujingshi <1471703928@qq.com>
2020-01-03 12:14:42 +0800

    update mobile

---
+++ template/mobile/FixedAssetsManagementSystem/app/start/start.js
@@ -0,0 +1,6 @@
+mui.ready(function() {
+	mui.init({});
+	mui.plusReady(function() {
+		mui.alert("固定资产管理系统", "Made in 刘叔", "刘叔最帅");
+	});
+});
--- template/mobile/README.md
+++ template/mobile/README.md
@@ -1,3 +1,5 @@
 # 移动端模板文件夹
 
 ## 放置移动端界面设计、html等
+
+* FixedAssetsManagementSystem 文件夹是5+App项目MUI框架
