cbb4c5f0d6d5ee2722535b6e4f67d2250c397a2f
liujingshi <1471703928@qq.com>
2020-01-04 16:04:30 +0800

    update test

--- template/pc/app/test/test.js
+++ template/pc/app/test/test.js
@@ -23,3 +23,28 @@ function test() {
         top: 600
     });
 }
+
+$('#table').bootstrapTable({
+    pagination: true,
+    search: true,
+    locale: "en",
+    columns: [{
+        field: 'id',
+        title: 'Item ID'
+    }, {
+        field: 'name',
+        title: 'Item Name'
+    }, {
+        field: 'price',
+        title: 'Item Price'
+    }],
+    data: [{
+        id: 1,
+        name: 'Item 1',
+        price: '$1'
+    }, {
+        id: 2,
+        name: 'Item 2',
+        price: '$2'
+    }]
+})
